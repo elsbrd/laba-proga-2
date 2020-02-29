85d8d9cb84427583680f68d63f72f63da6fae954
Tori-nkt <Tori-nkt@users.noreply.github.com>
2020-02-29 19:55:26 +0200

    Добавление списка бюджетников

--- laba proga 2/Program.cs
+++ laba proga 2/Program.cs
@@ -19,7 +19,9 @@ namespace laba_proga_2
 
             Console.WriteLine("Hello World!");
             string[] names = ReadText();
+            string[] bud = Budjetniki(names);
         }
+        public static int n, c = 0;
         static string[] ReadText()
         {
             string str = File.ReadAllText("students1.csv");
@@ -27,6 +29,10 @@ namespace laba_proga_2
             string[] num = new string[40];
             int k = 0;
             for (int i = 0; i < 40; i++)
+            n = Int32.Parse(wordSplit[0]); 
+            string[] num = new string[n];
+            int k = 1;
+            for (int i = 0; i < n; i++)
             {
                 try
                 {
@@ -38,8 +44,29 @@ namespace laba_proga_2
                 {
                     Console.WriteLine(ex.Message);
                 }
+                if (num[i].EndsWith("FALSE"))
+                {
+                    c++;
+                }
             }
             return num;
+        }
+        static string[] Budjetniki(string[] sp_vseh)
+        {
+            string[] sp_budj = new string[c];
+
+            Console.WriteLine("Список бюджетников: ");
+            int m = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (sp_vseh[i].EndsWith("FALSE"))
+                {
+                    sp_budj[m] = sp_vseh[i];
+                    Console.WriteLine(sp_budj[m]);
+                    m++;
+                }
+            }
+            return sp_budj;
         }
     }
 }
